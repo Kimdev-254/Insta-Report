6cd4d311e97c2d4ffa281c0389bc2e509d622bd2
Kimdev254 <bonifacekimani715@gmail.com>
2025-07-28 21:30:36 +0300

    refactor: Updated code after switching back to single theme

--- components/client-layout.tsx
+++ components/client-layout.tsx
@@ -1,16 +1,13 @@
 'use client'
 
 import { ErrorBoundary } from '@/components/ErrorBoundary'
-import { ThemeProvider } from '@/components/theme-provider'
 import { ToastProvider } from '@/components/toast-provider'
 
 export function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider>
-      <ErrorBoundary>
-        <ToastProvider />
-        {children}
-      </ErrorBoundary>
-    </ThemeProvider>
+    <ErrorBoundary>
+      <ToastProvider />
+      {children}
+    </ErrorBoundary>
   )
 }
