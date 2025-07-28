d46ede4076fa3649723b43bbc1459351d582dc78
Kimdev254 <bonifacekimani715@gmail.com>
2025-07-28 21:55:23 +0300

    fix: Removing ugly duplicate navbar

--- components/client-layout.tsx
+++ components/client-layout.tsx
@@ -3,12 +3,14 @@
 import { ErrorBoundary } from '@/components/ErrorBoundary'
 import { ThemeProvider } from '@/components/theme-provider'
 import { ToastProvider } from '@/components/toast-provider'
+import { Header } from '@/components/header'
 
 export function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider>
       <ErrorBoundary>
         <ToastProvider />
+        <Header />
         {children}
       </ErrorBoundary>
     </ThemeProvider>
