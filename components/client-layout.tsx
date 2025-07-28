351631afd88ece47344dd18541cbe9595d331b9f
Kimdev254 <bonifacekimani715@gmail.com>
2025-07-28 21:44:12 +0300

    fix: Typescript error fixed(syntax)

--- components/client-layout.tsx
+++ components/client-layout.tsx
@@ -1,13 +1,16 @@
 'use client'
 
 import { ErrorBoundary } from '@/components/ErrorBoundary'
+import { ThemeProvider } from '@/components/theme-provider'
 import { ToastProvider } from '@/components/toast-provider'
 
 export function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ErrorBoundary>
-      <ToastProvider />
-      {children}
-    </ErrorBoundary>
+    <ThemeProvider>
+      <ErrorBoundary>
+        <ToastProvider />
+        {children}
+      </ErrorBoundary>
+    </ThemeProvider>
   )
 }
